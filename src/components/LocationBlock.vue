8148989071cbe2d1b5154e8e4adc797601b77ccb
unknown <yourstruly.bill@gmail.com>
2024-01-11 18:46:43 +0400

    finish location block

--- src/components/LocationBlock.vue
+++ src/components/LocationBlock.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="location" id="location">
-    <div class="location__wrapper"></div>
-    <div class="location__presentation"></div>
+    <div class="location__wrapper">
+      <iframe
+        src="https://yandex.ru/map-widget/v1/?um=constructor%3A5d149da97894e0532815d59508f43e9a8197e43298c185ad2eac571682bda2b8&amp;source=constructor"
+        frameborder="0"
+        class="location__yandex"
+      >
+      </iframe>
+    </div>
+    <div class="location__presentation">
+      <div class="location__footer">
+        <a href="#home" class="location__footer__title">СПУТНИК | Кроссфит и Функционал в Самаре</a>
+        <img :src="require('@/assets/bckg_img/sputnik_logo.png')" alt="sputnik" class="location__footer__logo" />
+        <div class="location__footer__copy">© 2024 Sputnik | Crossfit</div>
+      </div>
+    </div>
   </div>
 </template>
 
